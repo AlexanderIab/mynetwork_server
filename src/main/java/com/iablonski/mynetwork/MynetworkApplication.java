67fbb39e047e833292030a0d25384dff160d10e1
Alexander Iablonski <alex.iablonski@gmail.com>
2023-05-09 14:45:04 +0300

    Start of the project

---
+++ src/main/java/com/iablonski/mynetwork/MynetworkApplication.java
@@ -0,0 +1,13 @@
+package com.iablonski.mynetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iablonski/mynetwork/MynetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iablonski.mynetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
